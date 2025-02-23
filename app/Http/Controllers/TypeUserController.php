b6dcdb9c158b4498914b0d40ded61c29181b7897
Kevin Ativon <87210876+Kevram73@users.noreply.github.com>
2025-02-23 20:47:39 +0000

    Update TypeUserController.php

--- app/Http/Controllers/TypeUserController.php
+++ app/Http/Controllers/TypeUserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\TypeUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class TypeUserController extends Controller
 {
