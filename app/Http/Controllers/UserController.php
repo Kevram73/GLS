3e4517d7666758a4f3f0fad4e78b1222fd614896
Kevram73 <eyramativon1@gmail.com>
2025-04-10 09:20:17 +0000

    cmmit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -138,4 +138,68 @@ class UserController extends Controller
         $users = User::where('type_user_id', $type_user_id)->get();
         return response()->json(UserResource::collection($users));
     }
+
+    public function add_client(Request $request)
+    {
+        // Validation des données entrantes
+        $validator = Validator::make($request->all(), [
+            'nom'       => 'required|string|max:255',
+            'prenom'    => 'required|string|max:255',
+            'email'     => 'required|string|email|max:255|unique:users,email',
+            'num_phone' => 'required|string|max:15|unique:users,num_phone'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        // Génération aléatoire du mot de passe
+        $pwd = rand(100000, 999999);
+
+        // Création de l'utilisateur
+        $user = User::create([
+            'nom'          => $request->nom,
+            'prenom'       => $request->prenom,
+            'email'        => $request->email,
+            'num_phone'    => $request->num_phone,
+            'type_user_id' => 3,
+            'password'     => Hash::make($pwd),
+            'actif'        => true,
+        ]);
+
+        // Envoi automatique du mot de passe à l'utilisateur
+        $this->sendPasswordToClient($user->email, $pwd);
+
+        return response()->json(['message' => 'Utilisateur créé avec succès', 'user' => $user]);
+    }
+
+    // Méthode privée pour l'envoi de l'email
+    private function sendPasswordToClient($email, $password)
+    {
+        Mail::raw("Votre mot de passe : $password", function ($message) use ($email) {
+            $message->to($email)->subject('Votre mot de passe');
+        });
+    }
+
+    public function check_email_availability($email)
+    {
+        $user = User::where('email', $email)->first();
+
+        if ($user) {
+            return response()->json(['available' => false]);
+        } else {
+            return response()->json(['available' => true]);
+        }
+    }
+    public function check_phone_availability($num_phone)
+    {
+
+        $user = User::where('num_phone', $num_phone)->first();
+
+        if ($user) {
+            return response()->json(['available' => false]);
+        } else {
+            return response()->json(['available' => true]);
+        }
+    }
 }
